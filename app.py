15c6fd4ce885c30a3909371396f0c873f6a19613
Mauricio Perez <mauricioperez1992@gmail.com>
2025-06-14 23:44:53 -0600

    Se sube configuraciones de puertos

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def obtener_datos():
     return Response(json_pretty, mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # 👈 render asigna este valor
+    app.run(host="0.0.0.0", port=port)        
